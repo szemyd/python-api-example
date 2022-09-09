4fcd034c016b30617f52b5a55b49b4b76ce42430
Daniel Szemerey <daniel@szemerey.eu>
2022-09-09 11:13:05 +0200

    feat: Changed it to their example.

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-from flask import Flask
-from flask_restful import Api
+# from flask import Flask
+# from flask_restful import Api
 
-from endpoints.users import  Users
-from endpoints.locations import Locations
+# from endpoints.users import  Users
+# from endpoints.locations import Locations
 
-app = Flask(__name__)
-api = Api(app)
-
-
-api.add_resource(Users, '/users')  # '/users' is our entry point for Users
-api.add_resource(Locations, '/locations')  # and '/locations' is our entry point for Locations
+# app = Flask(__name__)
+# api = Api(app)
 
 
-if __name__ == '__main__':
-    app.run()  # run our Flask app
+# api.add_resource(Users, '/users')  # '/users' is our entry point for Users
+# api.add_resource(Locations, '/locations')  # and '/locations' is our entry point for Locations
+
+
+# if __name__ == '__main__':
+#     app.run()  # run our Flask app
     
-app.run()
+# app.run()
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello Sammy!'
